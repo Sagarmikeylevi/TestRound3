11aa3265c273b7cb14587b0cc34908923484c15d
unknown <sagarmikeylevi@gmail.com>
2023-03-14 17:15:14 +0530

    This is a java code so please consider to use java compiler

---
+++ index.js
@@ -0,0 +1,12 @@
+// Given an array of Numbers return all possible permutations.
+// Input: [1,2,3]                                  output: [
+//  						[1,2,3]  
+//  						[1,3,2]  
+//  						[2,1,3]
+//  						[2,3,1]  
+//  						[3,1,2]  
+//  						[3,2,1]    
+// 					       ]
+
+
+console.log('hello');
