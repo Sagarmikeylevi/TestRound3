cbd0d1c9b5f885e132c1828974d8051af42fa4ce
unknown <sagarmikeylevi@gmail.com>
2023-03-14 17:16:35 +0530

    Forgot to save the code

--- index.js
+++ index.js
@@ -9,4 +9,65 @@
 // 					       ]
 
 
-console.log('hello');
+// This code is in java 
+
+
+public class Main {
+    
+    public static void reverse(int[] arr , int start , int end ) {
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start ++;
+            end --;
+        }
+    }
+    
+    public static void permutation(int[] arr , int num) {
+        while(num > 0) {
+            for(int v = 0; v<arr.length; v++) {
+                if(v == arr.length-1) System.out.print(arr[v]);
+                else System.out.print(arr[v] + ", ");
+                
+            }
+            System.out.println("");
+            
+            int i = arr.length - 2;
+            while(i >= 0 && arr[i] >= arr[i+1]) {
+                i--;
+            }
+            
+            if(i >= 0) {
+                int j = arr.length - 1;
+                while(arr[j] <= arr[i]) {
+                    j--;
+                }
+                
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            
+            reverse(arr , i+1 , arr.length-1);
+            
+            
+            
+            num--;
+        }
+    }
+    
+    public static int numOfPer(int n) {
+        int fact = 1;
+        for(int i = 2; i<=n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        int num = numOfPer(arr.length);
+        permutation(arr , num);
+    }
+}
